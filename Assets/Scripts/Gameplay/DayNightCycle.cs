26bb0983a694bc5778eb8d99212c7171039c542f
jeegnathebug <jeegnathebug@gmail.com>
2026-03-25 21:10:28 -0400

    Add day/night cycle to sky

---
+++ Assets/Scripts/Gameplay/DayNightCycle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class DayNightCycle : MonoBehaviour
+    {
+        public Camera mainCamera;
+
+        public Gradient skyGradient;
+
+        public float cycleDuration = 120f;
+
+        private float _time;
+
+        void Update()
+        {
+            _time += Time.unscaledDeltaTime;
+
+            float t = (_time % cycleDuration) / cycleDuration;
+
+            mainCamera.backgroundColor = skyGradient.Evaluate(t);
+        }
+    }
+}

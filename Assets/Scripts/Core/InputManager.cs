088da0224a5a5885614d8a62bca2cc13f0a86648
jeegnathebug <jeegnathebug@gmail.com>
2026-09-03 23:27:21 -0400

    found a bug :c

--- Assets/Scripts/Core/InputManager.cs
+++ Assets/Scripts/Core/InputManager.cs
@@ -46,6 +46,7 @@ namespace Core
         {
             switch (GameManager.State)
             {
+                // todo fixme the game is "Stopped" when in the MainMenu. this means pressing jump while in the main menu causes the game to start in the bg
                 case GameState.Stopped:
                     gameManager.StartGame();
                     break;
